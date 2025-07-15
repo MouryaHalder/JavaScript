49012c6f3a52e8cf837d53b0fca56723d91a1bad
MouryaHalder <112268253+MouryaHalder@users.noreply.github.com>
2025-07-16 02:33:27 +0530

    comparisons

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -2,8 +2,18 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
-console.log("2">1);
-console.log("02" > 1);
+// console.log("2">1);
+// console.log("02" > 1);
 
-console.log();
+// Avoid using == and != for comparisons, use === and !== instead
+console.log(null>0);
+console.log(null==0);
+console.log(null >=0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
 
+// String check
+console.log("2" == 2); // true
+console.log("2" === 2); // false checks data type
+console.log("2" !== 2); // true // checks data type
